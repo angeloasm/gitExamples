2d6991c3a0ef4853bcedd7c94ba5206c33d51e36
AxC1011 <angelocarraggi@gmail.com>
2019-01-13 22:05:20 +0100

    add & fix

--- main.cpp
+++ main.cpp
@@ -2,7 +2,12 @@
 #include "prova.h"
 #include "pluto.h"
 
+#include <iostream>
+
+using namespace std;
+
 int
 main(){
+	cout << "angelo" << endl;
 	return 0;
 }
--- prova.h
+++ prova.h
@@ -3,6 +3,7 @@ class Point2d
 public:
 	int x;
 	int y;
+	int z;
 	int getX();
 	void getY();
 	
